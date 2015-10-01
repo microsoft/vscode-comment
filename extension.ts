6c0525e939cb2e5ec465d7bcbb88bd73a78bce0b
Steven Clarke <stevencl@microsoft.com>
2015-10-01 15:53:31 +0100

    Implemented simple add doc comments

--- extension.ts
+++ extension.ts
@@ -1,22 +1,46 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode'; 
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate() { 
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+
 	console.log('Congratulations, your extension "addDocComments" is now active!'); 
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
+	vscode.commands.registerCommand('extension.addDocComments', () => {
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		var selection = vscode.window.getActiveTextEditor().getSelection();
+		var selectedText = vscode.window.getActiveTextEditor().getTextDocument().getTextInRange(selection);
+		
+		//This regex gives us the string containing the parameters. If there is more than one they will be
+		//separated by a comma
+		var selectedText = selectedText.match(/function[^(]*\(([^)]*)\)/)[1];
+		var paramList = selectedText.split(',');
+		if (paramList.length > 0) {		
+			var textToInsert = getParameters(selection, paramList);
+			
+			vscode.window.getActiveTextEditor().edit((editBuilder: vscode.TextEditorEdit) => {
+				var startLine = selection.start.line - 1;
+				var pos = new vscode.Position(startLine, 0);
+				editBuilder.insert(pos, textToInsert);
+			});
+		}
+
 	});
+	
+	
+	function getParameters(selection: vscode.Selection, paramList: string[]): string {
+		var textToInsert: string = "";
+		textToInsert = textToInsert + '/**\n    *';
+				
+		paramList.forEach(element => {
+			if (element != '') {
+				var s = element.trim();
+				var paramName = (s.split(':').shift());
+				var paramType = (s.split(':').pop());
+				textToInsert = textToInsert + '@param  ' + paramName + '\n' + '    *';
+			}
+		});
+		textToInsert = textToInsert + '/';
+		return textToInsert;
+	}
 }
