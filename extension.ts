72d090d00832e15667cf5a3cb8ce4ea847558067
Steven Clarke <stevencl@microsoft.com>
2015-10-23 14:57:42 +0100

    Check for JS or TS language

--- extension.ts
+++ extension.ts
@@ -6,33 +6,34 @@ export function activate() {
 
 
 	console.log('Congratulations, your extension "addDocComments" is now active!');
-	
-	
+
+
 
 	vscode.commands.registerCommand('extension.addDocComments', () => {
 
-		var selection = vscode.window.getActiveTextEditor().getSelection();
-		var selectedText = vscode.window.getActiveTextEditor().getTextDocument().getTextInRange(selection);
-		var firstBraceIndex = selectedText.indexOf('(');
-		selectedText = selectedText.slice(firstBraceIndex);
+		var lang = vscode.window.getActiveTextEditor().getTextDocument().getLanguageId();
+		if ((lang == "typescript") || (lang == 'javascript')) {
+			var selection = vscode.window.getActiveTextEditor().getSelection();
+			var selectedText = vscode.window.getActiveTextEditor().getTextDocument().getTextInRange(selection);
+			var firstBraceIndex = selectedText.indexOf('(');
+			selectedText = selectedText.slice(firstBraceIndex);
 
-		selectedText = functionParser.stripComments(selectedText);
-		
-		var returnText = functionParser.getReturns(selectedText);
-		
-		var params: functionParser.paramDeclaration[] = functionParser.getParameters(selectedText);
+			selectedText = functionParser.stripComments(selectedText);
 
-		if (params.length > 0) {
-			var textToInsert = functionParser.getParameterText(params, returnText);
-			vscode.window.getActiveTextEditor().edit((editBuilder: vscode.TextEditorEdit) => {
-				var startLine = selection.start.line - 1;
-				var pos = new vscode.Position(startLine, 0);
-				editBuilder.insert(pos, textToInsert);
-			}).then(()=>{
-				vscode.window.getActiveTextEditor().setSelection(selection.start);				
-			});
+			var returnText = functionParser.getReturns(selectedText);
 
+			var params: functionParser.paramDeclaration[] = functionParser.getParameters(selectedText);
 
+			if (params.length > 0) {
+				var textToInsert = functionParser.getParameterText(params, returnText);
+				vscode.window.getActiveTextEditor().edit((editBuilder: vscode.TextEditorEdit) => {
+					var startLine = selection.start.line - 1;
+					var pos = new vscode.Position(startLine, 0);
+					editBuilder.insert(pos, textToInsert);
+				}).then(() => {
+					vscode.window.getActiveTextEditor().setSelection(selection.start);
+				});
+			}
 		}
 	});
 }
