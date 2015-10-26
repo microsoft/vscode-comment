c6c1be897994ea316fe6231d1d33bd654f846451
Steven Clarke <steven_clarke@hotmail.com>
2015-10-26 12:17:58 +0000

    Try to set focus in editor

--- extension.ts
+++ extension.ts
@@ -14,6 +14,7 @@ export function activate() {
 		var lang = vscode.window.getActiveTextEditor().getTextDocument().getLanguageId();
 		if ((lang == "typescript") || (lang == 'javascript')) {
 			var selection = vscode.window.getActiveTextEditor().getSelection();
+			var startLine = selection.start.line - 1;
 			var selectedText = vscode.window.getActiveTextEditor().getTextDocument().getTextInRange(selection);
 			var firstBraceIndex = selectedText.indexOf('(');
 			selectedText = selectedText.slice(firstBraceIndex);
@@ -27,7 +28,6 @@ export function activate() {
 			if (params.length > 0) {
 				var textToInsert = functionParser.getParameterText(params, returnText);
 				vscode.window.getActiveTextEditor().edit((editBuilder: vscode.TextEditorEdit) => {
-					var startLine = selection.start.line - 1;
 					if (startLine == 0) {
 						//If the function declaration is on the first line in the editor we need to set startLine to first line
 						//and then add an extra newline at the end of the text to insert
@@ -37,7 +37,7 @@ export function activate() {
 					var pos = new vscode.Position(startLine, 1);
 					editBuilder.insert(pos, textToInsert);
 				}).then(() => {
-					vscode.window.getActiveTextEditor().setSelection(new vscode.Position(1, 1));
+					vscode.window.getActiveTextEditor().setSelection(new vscode.Position(startLine, 1));
 				});
 			}
 		}
